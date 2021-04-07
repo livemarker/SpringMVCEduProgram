eb5dc8f951f30dc24b5ad862a4e9c31838f817f1
Elovenko Nikita <livemarker@users.noreply.github.com>
2021-04-07 18:57:27 +0300

    Initial commit

---
+++ src/main/java/lessonMvc/config/MySpringMVCDispatcher.java
@@ -0,0 +1,20 @@
+package lessonMvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMVCDispatcher extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
